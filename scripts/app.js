630ac2622f01a827e217f26e4864ece246239ff9
jedgodsey <jed.godsey@gmail.com>
2020-09-12 11:50:51 -0700

    Commit 6 - Prime Numbers

--- scripts/app.js
+++ scripts/app.js
@@ -53,25 +53,37 @@ let sumArray = array => {
 
 console.log(sumArray([1, 2, 3, 4, 5, 6]));
 
-// "Commit 5 - Sum Array".
-// <hr>
-
 // ## 6. Prime Numbers
-// A Prime number is a number that is not evenly divisible by another number except 1 and itself. If you want to read more deeply about it, [go here](https://en.wikipedia.org/wiki/Prime_number).
+
 // To test whether a number is Prime, you only need to test as far as the **square root** of that number. This is advisable for optimization and testing large numbers.
 
 // ### Step One
 // Write a function called `checkPrime` that will test whether a number is Prime. The function will return true (Boolean) if Prime, false if not.
 // _Hint:_ Check every number up to the square root. To do this, try a _for loop_.
 
+let checkPrime = number => {
+    for (let i = 2; i <= Math.sqrt(number); i++) {
+        if (number % i === 0) {
+            return false;
+        }
+    }
+    return true;
+}
+console.log(checkPrime(97));
+console.log(checkPrime(63));
+
 // ### Step Two
 // Write another function called `printPrimes` that will print (console log) all the Primes up to an arbitrary limit. For example, if you invoke your function with `printPrimes(97)`, it will print all the Prime numbers up to and including 97.
 // This function can **call on** the previous `checkPrime` function.
-// </details>
+let printPrimes = limit => {
+    let allPrimes = [];
+    for (let i = 2; i <= limit; i++) {
+        checkPrime(i) ? allPrimes.push(i) : null;
+    }
+    console.log(allPrimes);
+}
+printPrimes(15);
 
-// <hr>
-// &#x1F534; **Commit your work.** <br>
-// The commit message should read: <br>
 // "Commit 6 - Prime Numbers".
 // <hr>
 
